51946e30a47ed4a3827a9c58173d9c7f59ddb45c
Nmahabir <natalia.mahabir@gmail.com>
2018-09-19 00:57:15 -0400

    psuedo for controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,12 +6,18 @@ var burger = require("../models/burger.js");
 
 router.get("/index", function(req, res) {
     burger.selectAll(function(data) {
-
+// code to generate the index sheet with text box
     })
 
 })
 
+// request to add burgers on the left side
+
+// request to add burgers that have been devoured to go to the right side
+
+// ajax code for the submit button 
 
 
 
-module.exports(router)
+
+module.exports(router);
